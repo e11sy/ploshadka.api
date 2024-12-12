bbcdaeeee8de0aaadb76ffb737c662d38eb4d03e
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-13 00:52:11 +0300

    added policies

---
+++ src/presentation/http/policies/authRequired.ts
@@ -0,0 +1,15 @@
+import type { PolicyContext } from '@presentation/http/types/PolicyContext.js';
+
+/**
+ * Policy to enforce user to be logged in
+ * @param context - Context object, containing Fatify request, Fastify reply and domain services
+ */
+export default async function authRequired(context: PolicyContext): Promise<void> {
+  const { request, reply } = context;
+
+  const { userId } = request;
+
+  if (userId === null) {
+    return await reply.unauthorized();
+  }
+}
