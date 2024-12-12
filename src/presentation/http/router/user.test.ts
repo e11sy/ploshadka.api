601c63acd6da9fde26505753e2f7bf29bf929e38
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-13 00:51:55 +0300

    test for user router implemented

---
+++ src/presentation/http/router/user.test.ts
@@ -0,0 +1,46 @@
+import { describe, test, expect } from 'vitest';
+
+describe('User API', () => {
+  describe('GET /user/myself', () => {
+    test('Returns user with status code 200 if user exists', async () => {
+      /**
+       * Truncate all tables, which are needed
+       * Restart autoincrement sequences for data to start with id 1
+       * @todo get rid of restarting database data in tests (move to beforeEach)
+       */
+      await global.db.truncateTables();
+
+      const user = await global.db.insertUser();
+
+      const accessToken = global.auth(user.id);
+
+      const response = await global.api?.fakeRequest({
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+        url: '/user/myself',
+      });
+
+      expect(response?.statusCode).toBe(200);
+
+      expect(response?.json()).toMatchObject({
+        name: user.name,
+        email: user.email,
+      });
+    });
+
+    test('Returns response with status 401 when user is not authorized', async () => {
+      const response = await global.api?.fakeRequest({
+        method: 'GET',
+        url: '/user/myself',
+      });
+
+      expect(response?.statusCode).toBe(401);
+
+      const body = response?.json();
+
+      expect(body.message).toBe('You must be authenticated to access this resource');
+    });
+  });
+});
