e7cf0401e8ee5e17e957cda7a167e27f86b0dab4
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-13 00:53:20 +0300

    add export to xml script

--- src/repository/storage/postgres/export/exportToXML.ts
+++ src/repository/storage/postgres/export/exportToXML.ts
@@ -16,49 +16,44 @@ const __dirname = path.dirname(__filename);
  */
 const tablesToExport = ['users', 'user_sessions', 'events'];
 
-/**
- * Method that exports all database tables into XML dumps directory
- * @param sequelize - instance of the sequelize
- */
-export async function exportToXML(): Promise<void> {
-  logger.info('exporting to XML...');
+logger.info('exporting to XML...');
 
-  try {
-    void sequelize.authenticate();
+try {
+  void sequelize.authenticate();
 
-    logger.info('connection has been established successfully.')
+  logger.info('connection has been established successfully.')
+
+  /**
+   * For each table export into xml format
+   */
+  for(let tableName of tablesToExport) {
+    /**
+     * Get result of the table_to_xml postgres method, for each of the specified tables
+     */
+    const [ result, _metadata ] = await sequelize.query<{"table_to_xml": any}[]>
+      ({
+        query: `SELECT table_to_xml('${tableName}', false, true, '')`,
+        values: ['table_to_xml'],
+      },
+      {
+        type: QueryTypes.SELECT,
+      });
 
     /**
-     * For each table export into xml format
+     * Check format of the result and write it into dumps directory
      */
-    for(let tableName of tablesToExport) {
-      /**
-       * Get result of the table_to_xml postgres method, for each of the specified tables
-       */
-      const [ result, _metadata ] = await sequelize.query<{"table_to_xml": any}[]>
-        ({
-          query: `SELECT table_to_xml('${tableName}', false, true, '')`,
-          values: ['table_to_xml'],
-        },
-        {
-          type: QueryTypes.SELECT,
-        });
+    if (result !== undefined && 'table_to_xml' in result && typeof result.table_to_xml === 'string') {
+      const targetPath = path.join(__dirname, '../../../../../dumps/xml', `${tableName}.xml`);
+      fs.writeFileSync(targetPath, result.table_to_xml, 'utf-8');
+    }
+  };
 
-      /**
-       * Check format of the result and write it into dumps directory
-       */
-      if (result !== undefined && 'table_to_xml' in result && typeof result.table_to_xml === 'string') {
-        const targetPath = path.join(__dirname, '../../../../../dumps', `${tableName}.xml`);
-        fs.writeFileSync(targetPath, result.table_to_xml, 'utf-8');
-      }
-    };
+  logger.info(`exports runned successfully`);
 
-    logger.info(`exports runned successfully`);
-
-  } catch(error) {
-    logger.error(`unable to connect to the database: ${error}`);
-  } finally {
-    await sequelize.close();
-  }
+} catch(error) {
+  logger.error(`unable to connect to the database: ${error}`);
+} finally {
+  await sequelize.close();
 }
+
 
