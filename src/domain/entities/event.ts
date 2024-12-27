479f18547d9852682221cdad86379d44b2baba66
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-27 13:08:37 +0300

    chore(entities): update court and event entities

--- src/domain/entities/event.ts
+++ src/domain/entities/event.ts
@@ -1,38 +1,50 @@
 import Court from './court.js';
 
 export default interface Event {
-    /**
-     * Event internal id
-     */
-    id: number,
+  /**
+   * Event internal id
+   */
+  id: number,
 
-    /**
-     * Id of court, where evert is based
-     */
-    courtId: Court['id'],
+  /**
+   * Id of court, where evert is based
+   */
+  courtId: Court['id'],
 
-    /**
-     * Name of the event
-     */
-    name: string,
+  /**
+   * Name of the event
+   */
+  name: string,
 
-    /**
-     * Description of the event
-     */
-    description: string,
+  /**
+   * Description of the event
+   */
+  description: string,
 
-    /**
-     * Sport type
-     */
-    sport: string,
+  /**
+   * Sport type
+   */
+  sport: string,
 
-    /**
-     * How many people already participate in the event and how many people can participate
-     */
-    peopleState: [number, number],
+  /**
+   * How many people already participate in the event and how many people can participate
+   */
+  peopleLimit: number,
 
-    /**
-     * Flag to mark event as visited or planned to visit
-     */
-    visited: boolean,
+  /**
+   * Date when event will be closed
+   */
+  expires_at: Date,
+}
+
+export interface EventPresentation extends Event{
+  /**
+   * How many people already participate in the event
+   */
+  peopleCount: number,
+
+  /**
+   * Is user participating in the event
+   */
+  isParticipating?: boolean,
 }
